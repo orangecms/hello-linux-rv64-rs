5eefda846580e2b9e8195cc2b37924abf577bce4
Daniel Maslowski <info@orangecms.org>
2021-12-31 00:28:48 +0100

    add meminfo and intro

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@
 
 use rustix::process::uname;
 use std::fs;
+use std::io::{BufRead, BufReader};
 
 fn main() {
+    println!("Let's have a look at your shiny RISC-V system! :)\n");
+
     let mut f = "/proc/cpuinfo";
     let cpuinfo = fs::read_to_string(f).expect("cpuinfo err");
     println!("cpuinfo:\n{}", cpuinfo);
+
+    f = "/proc/meminfo";
+    match fs::File::open(f) {
+        Ok(file) => {
+            let mut lines = BufReader::new(file).lines();
+            println!("meminfo:");
+            for _ in 0..3 {
+                if let Some(l) = lines.next() {
+                    println!("{}", l.unwrap());
+                }
+            }
+            println!();
+        }
+        Err(e) => println!("{}\n", e),
+    }
+
     f = "/proc/cmdline";
     let cmdline = fs::read_to_string(f).expect("cmdline err");
     println!("cmdline:\n{}", cmdline);
