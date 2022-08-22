877ce187fcec6175277a127b96db69e3bcb088cb
Matheus Catarino <matheus-catarino@hotmail.com>
2022-08-22 20:11:15 -0300

    meminfo countline

--- src/main.zig
+++ src/main.zig
@@ -22,25 +22,34 @@ pub fn main() !void {
         @compileError("Unsupported another OS");
     }
 
+    // detect memoryleak on (debug or ReleaseSafe)
+    var safeAlloc = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = safeAlloc.allocator();
+
     const cpuinfo = try filestream("/proc/cpuinfo", .{});
     defer cpuinfo.close();
     try stdout.print("cpuinfo:\n", .{});
     try stdoutFile.writeFileAll(cpuinfo, .{});
 
-    var buf: [84]u8 = undefined;
     const meminfo = try filestream("/proc/meminfo", .{});
     defer meminfo.close();
-    _ = try meminfo.readAll(&buf);
-    try stdout.print("meminfo:\n{s}\n", .{buf});
+    var file_buffer = std.io.bufferedReader(meminfo.reader());
+    var stream_in = file_buffer.reader();
 
+    try stdout.print("meminfo:\n", .{});
+    var buf: [1024]u8 = undefined;
+    var index: usize = 0;
+    while (try stream_in.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        defer index += 1;
+        try stdout.print("{s}\n", .{line});
+        if (index > 1) break;
+    }
+
+    try stdout.print("\n", .{});
     const cmdline = try filestream("/proc/cmdline", .{});
     defer cmdline.close();
     try stdout.print("cmdline:\n", .{});
     try stdoutFile.writeFileAll(cmdline, .{});
-
-    // detect leak memory on (debug or ReleaseSafe)
-    var safeAlloc = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = safeAlloc.allocator();
 
     const uname = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &[_][]const u8{ "uname", "-a" } });
     try stdout.print("kernel:\n{s}", .{uname.stdout});
