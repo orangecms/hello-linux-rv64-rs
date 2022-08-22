215de50313b401385c9d8b22f65a4f7e95ae79db
Matheus Catarino <matheus-catarino@hotmail.com>
2022-08-22 18:16:54 -0300

    Refactor code

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const filestream = std.fs.openFileAbsolute;
+const stdoutFile = std.io.getStdOut();
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
+    // .riscv32 - don't work:
+    // https://github.com/ziglang/zig/blob/c955379504d4866f9c474c50317b2a0da18ee631/lib/std/os/linux.zig#L35-L44
     const arch = switch (builtin.cpu.arch) {
         .x86_64 => "x86_64",
         .aarch64 => "ARM64",
-        .riscv64 => "RISC-V", // .riscv32 - don't work (need all features in baseline)
-        .mipsel => "MIPS32 Little Endian",
+        .riscv64 => "RISC-V",
+        .mipsel, .mips => "MIPS",
         else => @compileError("Unsupported CPU Architecture"),
     };
     try stdout.print("\nLet's have a look at your shiny {s} system! :)\n\n", .{arch});
@@ -18,29 +22,28 @@ pub fn main() !void {
         @compileError("Unsupported another OS");
     }
 
-    var buf: [100]u8 = undefined;
-    const cpuinfo = try std.fs.openFileAbsolute("/proc/cpuinfo", .{});
+    const cpuinfo = try filestream("/proc/cpuinfo", .{});
     defer cpuinfo.close();
-    _ = try cpuinfo.readAll(&buf);
-    try stdout.print("cpuinfo:\n{s}\n", .{buf});
+    try stdout.print("cpuinfo:\n", .{});
+    try stdoutFile.writeFileAll(cpuinfo, .{});
 
-    try stdout.print("\n\n", .{});
-
-    var mbuf: [84]u8 = undefined;
-    const meminfo = try std.fs.openFileAbsolute("/proc/meminfo", .{});
+    var buf: [84]u8 = undefined;
+    const meminfo = try filestream("/proc/meminfo", .{});
     defer meminfo.close();
-    _ = try meminfo.readAll(&mbuf);
-    try stdout.print("meminfo:\n{s}\n", .{mbuf});
+    _ = try meminfo.readAll(&buf);
+    try stdout.print("meminfo:\n{s}\n", .{buf});
 
     var cmdbuf: [93]u8 = undefined;
-    const cmdline = try std.fs.openFileAbsolute("/proc/cmdline", .{});
+    const cmdline = try filestream("/proc/cmdline", .{});
     defer cmdline.close();
+
     _ = try cmdline.readAll(&cmdbuf);
     try stdout.print("cmdline:\n{s}\n", .{cmdbuf});
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+    // detect leak memory on (debug or ReleaseSafe)
+    var safeAlloc = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = safeAlloc.allocator();
+
     const uname = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &[_][]const u8{ "uname", "-a" } });
     try stdout.print("kernel:\n{s}", .{uname.stdout});
 }
